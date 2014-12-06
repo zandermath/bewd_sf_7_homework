a08809c6864ca8b06a221e4b0517242fb005c6a1
nsuitter <nsuitter@saleforce.com>
2014-12-06 06:24:51 -0800

    Fizz buzz quiz

---
+++ nathan_suitter/qf_fizz_buzz.rb
@@ -0,0 +1,37 @@
+# method to check for equality
+def assert_equal(expected, actual)
+  raise "Expected #{expected} to match #{actual}" unless expected == actual
+end
+
+# Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+#
+# Your code here
+
+def fizzbuzz(num)
+	if (num % 5 == 0) && (num % 3 == 0)
+		"FizzBuzz"
+	elsif (num % 5 == 0) && (num % 3 != 0)
+		"Buzz"
+	elsif (num % 3 == 0) && (num % 5 != 0)
+		"Fizz"
+	else
+		num
+	end
+end
+		 	 
+
+# Tests
+# fizzbuzz(15)
+# assert_equal fizzbuzz(1), 1
+# assert_equal fizzbuzz(3), "Fizz"
+# assert_equal fizzbuzz(4), 4
+# assert_equal fizzbuzz(50), "Buzz"
+# assert_equal fizzbuzz(15), "FizzBuzz"
+# assert_equal fizzbuzz(5175), "FizzBuzz"
+
+# uncomment this code when your tests pass
+1.upto(100) do |number|
+ puts fizzbuzz(number)
+end
