3a0f6129b262136037864191b08d22228f221bae
nsuitter <nsuitter@saleforce.com>
2014-12-30 16:48:48 -0800

    Finished step 5.10 of rails blog tutorial

--- nathan_suitter/blog/config/routes.rb
+++ nathan_suitter/blog/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  root 'welcome#index'
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
