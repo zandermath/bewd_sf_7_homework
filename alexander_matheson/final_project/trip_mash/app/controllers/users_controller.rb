81d509fecaa15e007a79f142dcf18b3979b2b566
zandermath <a.w.matheson@gmail.com>
2015-02-14 14:03:16 -0800

    Changed to postgresql

--- alexander_matheson/final_project/trip_mash/app/controllers/users_controller.rb
+++ alexander_matheson/final_project/trip_mash/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:following, :followers]
+  before_action :authenticate_user!, only: [:index, :following, :followers]
+
+  def index
+    @users = User.all
+  end
 
   def show
   	@user = User.find(params[:id])
